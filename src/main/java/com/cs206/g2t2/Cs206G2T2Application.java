23ec808f951f1422b2f089b7dcb35b455ac4b0ec
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-02-05 22:14:10 +0800

    Initial commit

---
+++ src/main/java/com/cs206/g2t2/Cs206G2T2Application.java
@@ -0,0 +1,13 @@
+package com.cs206.g2t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs206G2T2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs206G2T2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs206/g2t2/Cs206G2T2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs206.g2t2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs206G2T2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
