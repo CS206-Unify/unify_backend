285816b77e3dbb45b5ff7f133dfc93a396762210
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-02-22 23:22:40 +0800

    feat: Updated variables in UpdateProfileRequest to Unify requirements

---
+++ src/main/java/com/cs206/g2t2/data/request/auth/UpdateProfileRequest.java
@@ -0,0 +1,28 @@
+package com.cs206.g2t2.data.request.auth;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateProfileRequest implements UserRequest {
+
+    //imageString stores the S3 image link, can be null if there is no image
+    private String imageString;
+
+    //country stores the user's country of origin/base
+    @NotBlank(message = "Country is required")
+    private String country;
+
+    //language stores the user's preferred language
+    @NotBlank(message = "Language is required")
+    private String language;
+
+    // Not sure if integration with Google, Supercell, Garena and Moonton requires additional data to be saved
+}
+
