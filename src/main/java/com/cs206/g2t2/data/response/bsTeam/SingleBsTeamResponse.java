f03380d5067c77d6de6c8749bdf9dcf92277efb7
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-02-25 22:07:45 +0800

    feat: Created Response classes for BsTeam to be returned

---
+++ src/main/java/com/cs206/g2t2/data/response/bsTeam/SingleBsTeamResponse.java
@@ -0,0 +1,16 @@
+package com.cs206.g2t2.data.response.bsTeam;
+
+import com.cs206.g2t2.data.response.Response;
+import com.cs206.g2t2.models.BsTeam;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SingleBsTeamResponse implements Response {
+    protected BsTeam bsTeam;
+}
