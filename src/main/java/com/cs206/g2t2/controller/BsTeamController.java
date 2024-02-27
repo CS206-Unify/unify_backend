9e9a87e351a7ab82013732f60706f62e9b907ba0
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-02-27 15:46:20 +0800

    feat: Set up API endpoint for TeamUpdate

--- src/main/java/com/cs206/g2t2/controller/BsTeamController.java
+++ src/main/java/com/cs206/g2t2/controller/BsTeamController.java
@@ -1,6 +1,7 @@
 package com.cs206.g2t2.controller;
 
 import com.cs206.g2t2.data.request.team.TeamCreationRequest;
+import com.cs206.g2t2.data.request.team.TeamUpdateRequest;
 import com.cs206.g2t2.data.response.Response;
 import com.cs206.g2t2.service.services.BsTeamService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -10,10 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -31,6 +29,19 @@ public class BsTeamController {
 
         //Create team using createTeam method in bsTeamService
         Response response = bsTeamService.createBsTeam(request, username);
+
+        //If successful, the response is encapsulated with HTTP code of 200(ok) and contains the User object
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
+    @PutMapping("/team")
+    public ResponseEntity<Response> updateTeam(@Valid @RequestBody TeamUpdateRequest request,
+                                               @AuthenticationPrincipal UserDetails userDetails) {
+        // Get the username from the userDetails of the authenticated user
+        String username = userDetails.getUsername();
+
+        //Create team using createTeam method in bsTeamService
+        Response response = bsTeamService.updateBsTeam(request, username);
 
         //If successful, the response is encapsulated with HTTP code of 200(ok) and contains the User object
         return new ResponseEntity(response, HttpStatus.OK);
