9f6458c978202cba5a645e36a51a88c9f0af98ac
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-02-28 16:27:22 +0800

    feat: Updated deleteProfile logic to include transferring ownership and deleting teams

--- src/main/java/com/cs206/g2t2/service/serviceImpl/ProfileServiceImpl.java
+++ src/main/java/com/cs206/g2t2/service/serviceImpl/ProfileServiceImpl.java
@@ -6,18 +6,27 @@ import com.cs206.g2t2.data.request.profile.UpdateGameProfileRequest;
 import com.cs206.g2t2.data.response.Response;
 import com.cs206.g2t2.data.response.common.SuccessResponse;
 import com.cs206.g2t2.data.response.user.SingleUserResponse;
+import com.cs206.g2t2.exceptions.notFound.MemberNotFoundException;
+import com.cs206.g2t2.exceptions.notFound.TeamNotFoundException;
 import com.cs206.g2t2.exceptions.notFound.UsernameNotFoundException;
+import com.cs206.g2t2.models.BsTeam;
+import com.cs206.g2t2.models.TeamMember;
 import com.cs206.g2t2.models.User;
+import com.cs206.g2t2.repository.BsTeamRepository;
 import com.cs206.g2t2.repository.UserRepository;
 import com.cs206.g2t2.service.services.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProfileServiceImpl implements ProfileService {
 
     private final UserRepository userRepository;
+    private final BsTeamRepository bsTeamRepository;
 
     /**
      * @param username a String object containing the username to be searched
@@ -110,14 +119,97 @@ public class ProfileServiceImpl implements ProfileService {
     }
 
     /**
+     * Private Helper: Find team member in the bsTeam
+     * If team member not found, throws MemberNotFoundException
+     *
+     * @param user a User object containing the user to be found
+     * @param bsTeam a BSTeam object that stores the bsTeam to find user
+     * @return TeamMember object containing the user's id
+     */
+    private TeamMember findTeamMember(User user, BsTeam bsTeam) throws MemberNotFoundException {
+        //Searches through the memberList for the teamMember
+        for (TeamMember teamMember : bsTeam.getMemberList()) {
+            //If TeamMember object id tallies with userId
+            if (teamMember.getUserId().equals(user.get_id())) {
+                //Returns TeamMember object if found
+                return teamMember;
+            }
+        }
+        //Throws MemberNotFoundException if user cannot be found in the memberList
+        throw new MemberNotFoundException(user.getUsername(), bsTeam.getTeamName());
+    }
+
+    /**
+     * Private Helper: Transfers BsTeam ownership from user to the next best successor based on role, then join date
+     * If successor not found, deletes the bsTeam from repository
+     *
+     * @param user a User object containing the user holding ownership
+     * @param userMember a TeamMember object of the user to be deleted
+     * @param bsTeam a BSTeam object that stores the bsTeam to perform transfer of ownership
+     */
+    private void transferBsTeamOwnership(User user, TeamMember userMember, BsTeam bsTeam) {
+        //Stores the team member to be promoted to owner
+        TeamMember successor = null;
+        //Stores the role of the team member to be promoted to owner
+        TeamMember.Role successorRole = TeamMember.Role.MEMBER;
+        //Stores the joinDate of the successor
+        LocalDateTime joinDate = LocalDateTime.now();
+
+        //Searches through the memberList for a successor by finding the highest role, followed by longest serving
+        for (TeamMember teamMember : bsTeam.getMemberList()) {
+            //Only looks at teamMember which are not the user
+            if (!teamMember.getUserId().equals(user.get_id())) {
+                //If teamMember's role is higher than the current successor's role
+                //Or teamMember's role is equals to the current successor's role and joinDate is earlier
+                if (teamMember.getRole().isHigherRole(successorRole) ||
+                   (teamMember.getRole().isEqualRole(successorRole) && teamMember.getJoinDate().isBefore(joinDate))) {
+                    //Change current successor to the team member
+                    successor = teamMember;
+                    successorRole = teamMember.getRole();
+                    joinDate = teamMember.getJoinDate();
+                }
+            }
+        }
+
+        //If no such member can be found, delete team
+        if (successor == null) {
+            bsTeamRepository.deleteById(bsTeam.get_id());
+            return;
+        }
+
+        //Perform transfer ownership
+        successor.setRole(TeamMember.Role.OWNER);
+
+        //Delete user from memberList
+        bsTeam.getMemberList().remove(userMember);
+
+        //Save bsTeam into bsTeamRepository
+        bsTeamRepository.save(bsTeam);
+    }
+
+    /**
      * @param username a String object containing the username of user to be deleted
      * @return SuccessResponse "User has been deleted successfully"
      */
     @Override
     public Response deleteUserProfile(String username) throws UsernameNotFoundException {
         //Finds user in repository, else throws UsernameNotFoundException
-        userRepository.findByUsername(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        //Find all teams that user is in
+        List<String> bsTeamList = user.getTeams();
+        for (String bsTeamId : bsTeamList) {
+            //Obtains team from bsTeamRepository
+            BsTeam bsTeam = bsTeamRepository.findBy_id(bsTeamId)
+                    .orElseThrow(() -> new TeamNotFoundException(bsTeamId));
+            //Finds TeamMember from the bsTeam's member list
+            TeamMember userMember = findTeamMember(user, bsTeam);
+            //Performs transfer ownership if user is an owner and delete bsTeam if no successor found
+            if (userMember.getRole() == TeamMember.Role.OWNER) {
+                transferBsTeamOwnership(user, userMember, bsTeam);
+            }
+        }
 
         //Deletes user from repository
         userRepository.deleteByUsername(username);
