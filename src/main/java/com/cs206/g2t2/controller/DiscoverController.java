0d8761ab42b4be4ebc278dfe1a69e19ee8e54d2d
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-02-28 21:18:47 +0800

    feat: Created API end points for discover APIs

---
+++ src/main/java/com/cs206/g2t2/controller/DiscoverController.java
@@ -0,0 +1,61 @@
+package com.cs206.g2t2.controller;
+
+import com.cs206.g2t2.data.response.Response;
+import com.cs206.g2t2.service.services.DiscoverService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/discover")
+@RequiredArgsConstructor
+@SecurityRequirement(name = "bearerAuth")
+public class DiscoverController {
+
+    private final DiscoverService discoverService;
+
+    @GetMapping("/team")
+    public ResponseEntity<Response> discoverTeams(@RequestParam(required = false) String region,
+                                                  @RequestParam(required = false) String language,
+                                                  @RequestParam(required = false) Integer trophies,
+                                                  @RequestParam(required = false) Integer threeVThreeWins,
+                                                  @RequestParam(required = false) Integer twoVTwoWins,
+                                                  @RequestParam(required = false) Integer soloWins,
+                                                  @RequestParam Integer pageSize,
+                                                  @RequestParam Integer pageNumber,
+                                                  @AuthenticationPrincipal UserDetails userDetails) {
+        // Get the username from the userDetails of the authenticated user
+        String username = userDetails.getUsername();
+
+        //Create team using createTeam method in bsTeamService
+        Response response = discoverService.discoverTeam(username,region, language, trophies, threeVThreeWins, twoVTwoWins, soloWins, pageSize, pageNumber);
+
+        //If successful, the response is encapsulated with HTTP code of 201(created) and contains the BsTeam object
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<Response> discoverProfile(@RequestParam(required = false) String region,
+                                                    @RequestParam(required = false) String language,
+                                                    @RequestParam(required = false) Integer trophies,
+                                                    @RequestParam(required = false) Integer threeVThreeWins,
+                                                    @RequestParam(required = false) Integer twoVTwoWins,
+                                                    @RequestParam(required = false) Integer soloWins,
+                                                    @RequestParam Integer pageSize,
+                                                    @RequestParam Integer pageNumber,
+                                                    @AuthenticationPrincipal UserDetails userDetails) {
+        // Get the username from the userDetails of the authenticated user
+        String username = userDetails.getUsername();
+
+        //Create team using createTeam method in bsTeamService
+        Response response = discoverService.discoverProfile(username, region, language, trophies, threeVThreeWins, twoVTwoWins, soloWins, pageSize, pageNumber);
+
+        //If successful, the response is encapsulated with HTTP code of 201(created) and contains the BsTeam object
+        return new ResponseEntity(response, HttpStatus.CREATED);
+    }
+}
+
