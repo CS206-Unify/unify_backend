22ad87ad8349700c54a0426ac9870393ef3bc1d5
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-03-02 03:45:29 +0800

    feat: Implemented logic to retreive Battle Logs

--- src/main/java/com/cs206/g2t2/service/serviceImpl/BrawlStarsAPIServiceImpl.java
+++ src/main/java/com/cs206/g2t2/service/serviceImpl/BrawlStarsAPIServiceImpl.java
@@ -1,16 +1,19 @@
 package com.cs206.g2t2.service.serviceImpl;
 
 import com.cs206.g2t2.data.response.Response;
+import com.cs206.g2t2.data.response.brawlStars.BsPlayerBattleLogResponse;
 import com.cs206.g2t2.data.response.brawlStars.BsPlayerResponse;
 import com.cs206.g2t2.data.response.common.SuccessResponse;
 import com.cs206.g2t2.exceptions.notFound.BsPlayerTagNotFoundException;
 import com.cs206.g2t2.exceptions.notFound.UsernameNotFoundException;
 import com.cs206.g2t2.models.User;
+import com.cs206.g2t2.models.brawlStarsAPI.battleLog.Battle;
+import com.cs206.g2t2.models.brawlStarsAPI.battleLog.BattleLog;
+import com.cs206.g2t2.models.brawlStarsAPI.battleLog.BattleLogList;
 import com.cs206.g2t2.models.brawlStarsAPI.playerInfo.Player;
 import com.cs206.g2t2.repository.UserRepository;
 import com.cs206.g2t2.service.services.BrawlStarsAPIService;
 import com.google.gson.Gson;
-import io.swagger.v3.oas.models.links.Link;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,7 +25,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.util.LinkedHashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +33,8 @@ public class BrawlStarsAPIServiceImpl implements BrawlStarsAPIService {
     private static final String brawlStarsInstanceUrl = "https://api.brawlstars.com/v1";
 
     private static final String playerInfoPath = "/players/%23";
+    
+    private static final String battleLogPath = "/battlelog";
 
     @Value("${unify.backend.app.brawlStarsKey}")
     private String apiKey;
@@ -136,6 +140,100 @@ public class BrawlStarsAPIServiceImpl implements BrawlStarsAPIService {
         //Returns SuccessResponse once the Stats has been updated
         return SuccessResponse.builder()
                 .message("User's Brawl Star Stats has been updated successfully")
+                .build();
+    }
+
+    /**
+     * Private Helper: Perform API call to obtain Brawl Star Player Battle Log
+     *
+     * @param username a String containing the username of the user obtained from the token
+     * @return BattleLogList object containing user's battleLog
+     */
+    private BattleLogList findBattleLogListFromUsername(String username) {
+        //Obtains playerTag from username
+        String playerTag = getPlayerTagFromUser(username);
+
+        //Sets headers values of request and constructs a HttpEntity
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(apiKey);
+        headers.set("Accept", "application/json");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        //Constructs the URI for the API call to obtain playerInfo
+        String url = brawlStarsInstanceUrl + playerInfoPath + playerTag + battleLogPath;
+        URI uri = URI.create(url);
+
+        //Make a GET call to the API
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                entity,
+                String.class
+        );
+
+        //Converts the String into gson using serialized annotation
+        BattleLogList battleLogList = gson.fromJson(responseEntity.getBody(), BattleLogList.class);
+
+        //Returns BattleLog in ResponseEntity
+        return battleLogList;
+    }
+
+    /**
+     * @param username a String containing the username of the user obtained from the token
+     * @return BsPlayerBattleLogResponse containing the BattleLogList object in the body
+     */
+    @Override
+    public Response getBsBattleLog(String username) {
+        //Finds the Brawl Stars battleLog from username
+        BattleLogList battleLogList = findBattleLogListFromUsername(username);
+
+        //Return a BsPlayerResponse
+        return BsPlayerBattleLogResponse.builder()
+                .battleLogList(battleLogList)
+                .build();
+    }
+
+    /**
+     * @param username a String containing the username of the user obtained from the token
+     * @return SuccessResponse "User's Brawl Star Battle Log has been updated successfully"
+     */
+    @Override
+    public Response updateBsBattleLog(String username) {
+        //Finds the Brawl Stars battleLog from username
+        BattleLogList battleLogList = findBattleLogListFromUsername(username);
+
+        //Obtain user from userRepository and throws UsernameNotFoundException if user not found
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        //Finds wins,losses and draws from past 25 battles
+        int wins = 0;
+        int losses = 0;
+        int draws = 0;
+
+        //Loops through all 25 battleLogs
+        for (BattleLog battleLog : battleLogList.getItems()) {
+            Battle battle = battleLog.getBattle();
+            if (battle.getResult().equals("victory")) {
+                wins++;
+            } else if (battle.getResult().equals("defeat")) {
+                losses++;
+            } else if (battle.getResult().equals("draw")) {
+                draws++;
+            }
+        }
+
+        //Updates the variables in the field
+        user.setWins(wins);
+        user.setLosses(losses);
+        user.setDraws(draws);
+
+        //Saves user back into userRepository
+        userRepository.save(user);
+
+        //Returns SuccessResponse once the Stats has been updated
+        return SuccessResponse.builder()
+                .message("User's Brawl Star Battle Log has been updated successfully")
                 .build();
     }
 }
--- src/main/java/com/cs206/g2t2/service/services/BrawlStarsAPIService.java
+++ src/main/java/com/cs206/g2t2/service/services/BrawlStarsAPIService.java
@@ -1,6 +1,7 @@
 package com.cs206.g2t2.service.services;
 
 import com.cs206.g2t2.data.response.Response;
+import com.cs206.g2t2.data.response.brawlStars.BsPlayerBattleLogResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,4 +24,22 @@ public interface BrawlStarsAPIService {
      * @return SuccessResponse "User's Brawl Star Stats has been updated successfully"
      */
     Response updateBsStats(String username);
+
+    /**
+     * Finds a user's Brawl Star player battle log from external API.
+     * If username cannot be found in the repository, throw a UsernameNotFoundException.
+     *
+     * @param username a String containing the username of the user obtained from the token
+     * @return BsPlayerBattleLogResponse containing the BattleLog object in the body
+     */
+    Response getBsBattleLog(String username);
+
+    /**
+     * Updates a user's Brawl Star player battle log from external API into the repository.
+     * If username cannot be found in the repository, throw a UsernameNotFoundException.
+     *
+     * @param username a String containing the username of the user obtained from the token
+     * @return SuccessResponse "User's Brawl Star Battle Log has been updated successfully"
+     */
+    Response updateBsBattleLog(String username);
 }
