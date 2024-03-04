b7cf31cf492964198d5572ca6bd8246c36d6d3de
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-03-04 19:44:30 +0800

    chore: Clean up unused imports

--- src/main/java/com/cs206/g2t2/controller/RestExceptionHandler.java
+++ src/main/java/com/cs206/g2t2/controller/RestExceptionHandler.java
@@ -1,20 +1,14 @@
 package com.cs206.g2t2.controller;
 
-import com.amazonaws.services.xray.model.Http;
 import com.cs206.g2t2.exceptions.badRequest.BadRequestException;
-import com.cs206.g2t2.exceptions.badRequest.DuplicatedEmailException;
-import com.cs206.g2t2.exceptions.badRequest.DuplicatedTeamNameException;
-import com.cs206.g2t2.exceptions.badRequest.DuplicatedUsernameException;
 import com.cs206.g2t2.exceptions.brawlStarsApi.ExternalAPIException;
 import com.cs206.g2t2.exceptions.forbidden.ForbiddenException;
-import com.cs206.g2t2.exceptions.notFound.*;
-import com.cs206.g2t2.exceptions.unauthorized.InvalidCredentialsException;
+import com.cs206.g2t2.exceptions.notFound.NotFoundException;
 import com.cs206.g2t2.exceptions.unauthorized.UnauthorizedException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,7 +16,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
