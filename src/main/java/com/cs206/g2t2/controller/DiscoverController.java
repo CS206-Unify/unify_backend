1688182db3cc3d2e444a98ab6230d401e974784d
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2024-03-19 23:07:42 +0800

    feat: Implemented logic for homepage discover

--- src/main/java/com/cs206/g2t2/controller/DiscoverController.java
+++ src/main/java/com/cs206/g2t2/controller/DiscoverController.java
@@ -38,6 +38,26 @@ public class DiscoverController {
         return new ResponseEntity(response, HttpStatus.OK);
     }
 
+    @GetMapping("/home/team")
+    public ResponseEntity<Response> discoverHomePageBsTeam(@RequestParam(required = false) String region,
+                                                   @RequestParam(required = false) Integer trophies,
+                                                   @RequestParam(required = false) Integer threeVThreeWins,
+                                                   @RequestParam(required = false) Integer twoVTwoWins,
+                                                   @RequestParam(required = false) Integer soloWins,
+                                                   @RequestParam Integer pageSize,
+                                                   @RequestParam Integer pageNumber,
+                                                   @AuthenticationPrincipal UserDetails userDetails) {
+        // Get the username from the userDetails of the authenticated user
+        String username = userDetails.getUsername();
+
+        //Create team using createTeam method in bsTeamService
+        Response response = discoverService.discoverHomePageBsTeam(username, region, trophies, threeVThreeWins,
+                twoVTwoWins, soloWins, pageSize, pageNumber);
+
+        //If successful, the response is encapsulated with HTTP code of 201(created) and contains the BsTeam object
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
     @GetMapping("/profile")
     public ResponseEntity<Response> discoverBsProfile(@RequestParam(required = false) String region,
                                                     @RequestParam(required = false) String language,
@@ -54,6 +74,27 @@ public class DiscoverController {
         //Create team using createTeam method in bsTeamService
         Response response = discoverService.discoverBsProfile(username, region, language, trophies, threeVThreeWins,
                                                             twoVTwoWins, soloWins, pageSize, pageNumber);
+
+        //If successful, the response is encapsulated with HTTP code of 201(created) and contains the BsTeam object
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/home/profile")
+    public ResponseEntity<Response> discoverHomePageBsProfile(@RequestParam(required = false) String region,
+                                                      @RequestParam(required = false) String language,
+                                                      @RequestParam(required = false) Integer trophies,
+                                                      @RequestParam(required = false) Integer threeVThreeWins,
+                                                      @RequestParam(required = false) Integer twoVTwoWins,
+                                                      @RequestParam(required = false) Integer soloWins,
+                                                      @RequestParam Integer pageSize,
+                                                      @RequestParam Integer pageNumber,
+                                                      @AuthenticationPrincipal UserDetails userDetails) {
+        // Get the username from the userDetails of the authenticated user
+        String username = userDetails.getUsername();
+
+        //Create team using createTeam method in bsTeamService
+        Response response = discoverService.discoverHomePageBsProfile(username, region, language, trophies, threeVThreeWins,
+                twoVTwoWins, soloWins, pageSize, pageNumber);
 
         //If successful, the response is encapsulated with HTTP code of 201(created) and contains the BsTeam object
         return new ResponseEntity(response, HttpStatus.OK);
